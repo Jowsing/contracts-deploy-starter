a37a4a890dedb98ff0e75fd5cdc2bcd4c46b3932
StevenJobs <StevenJobs@apple.com>
2021-04-01 00:13:52 +0800

    add test contract

---
+++ contracts/Test.sol
@@ -0,0 +1,7 @@
+pragma solidity 0.6.12;
+
+contract Test {
+    constructor() public {}
+
+    function test() external {}
+}
--- deploy/index.js
+++ deploy/index.js
@@ -5,6 +5,13 @@ async function main() {
 
 	// TODO: code your deploy
 	console.log('hello deployer');
+
+	const TestJSON = require('../build/contracts/Test.json');
+	const TestContract = await deployContract(TestJSON);
+	console.log('Test Contract deployed at address: ', TestContract.options.address);
+
+	const tx = await dispatchContract(TestJSON.abi, TestContract.options.address, methods => methods.test());
+	console.log('dispatch Test Contract result: ', tx);
 }
 
 main();
